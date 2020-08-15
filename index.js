53b55df6ce0dab94d0ee15f69ea59dfe124d17bf
Nusiba Ibrahim <ainusiba@gmail.com>
2020-08-15 21:51:36 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,4 +10,18 @@ function handleClick(e) {
 /* End Given Code, don't edit above here...*/
 
 /* Write your implementation of greet() */
+function greet(time){
+  if(time.parseInt < 12){
+    return `Good Morning`;
+  } else if(time >12 && time < 17)
+  {
+    return `Good Afternoon`;
+    
+  }else (time > 17 && time > 24){
+    return `Good Evening`;
+  }
+}
 /* Write your implementation of displayMessage() */
+function displayMessage(){
+
+}
